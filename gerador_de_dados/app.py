5d0c002da971d78b57c89258bd0ebab85608a3be
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-03-28 16:33:00 -0300

    atualizando

--- gerador_de_dados/app.py
+++ gerador_de_dados/app.py
@@ -39,30 +39,47 @@ while True:
     print()
     print('Escolha quais dados devem ser gerados entre: ')
     print('Nome(1), E-mail(2), telefone(3), cidade(4) e/ou estado(5).')
-
-    opcao = int(input('Digite o número respectivo à escolha: '))
     print()
 
-    if opcao == 1:
-        print(random.choice(nomes))
-        print()
-    elif opcao == 2:
-        print(random.choice(emails))
-        print()
-    elif opcao == 3:
-        print(random.choice(telefones))
-        print()
-    elif opcao == 4:
-        print(random.choice(cidades))
-        print()
-    elif opcao == 5:
-        print(random.choice(estados))
-        print()
+    opcoes = input('Digite o número respectivo à escolha ou "parar" \npara sair do sistema: ')
+    print()
 
-    elif opcao == 'parar':
-        print('Você está saindo do sistema...')
-        break
+    if opcoes.lower() == 'parar':
+            print('Você está saindo do sistema...')
+            break
 
-    else:
-        print('Por favor, digite um número entre 1 e 5.')
-        print()
+    dados = []
+
+
+    for opcao in opcoes:
+        if opcao == '1':
+            dados.append(random.choice(nomes))
+        elif opcao == '2':
+            dados.append(random.choice(emails))
+        elif opcao == '3':
+            dados.append(random.choice(telefones))
+        elif opcao == '4':
+            dados.append(random.choice(cidades))
+        elif opcao == '5':
+            dados.append(random.choice(estados))
+        else:
+            print('Por favor, digite um número entre 1 e 5.')
+            print()
+
+    print("Dados gerados: \n")
+    for dado in dados:
+        print(dado)
+    print()
+
+    while opcoes != 'parar':
+        if opcao == '1' or '2' or '3' or '4' or '5':
+            salvar = input('Você deseja salvar os dados em um arquivo .txt? [s]im ou [n]ao: ')
+            if salvar.lower() == 's':
+                with open("arquivo.txt", "a") as arquivo:
+                    for dado in dados:
+                        arquivo.write(opcao + '\n')
+                    print(f'Os dados foram salvos em "arquivo.txt". ')
+                print()
+                break
+            elif salvar.lower() == 'n':
+                break
